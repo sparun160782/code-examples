6f4d2e64610167e9bc03160e10a00b0b8705eae2
sparun160782 <sparun1607@gmail.com>
2021-11-24 22:35:19 +0530

    Update KafkaProducerConfig.java
    The VALUE_SERIALIZER_CLASS_CONFIG was missing

--- spring-boot/spring-boot-kafka/src/main/java/io/reflectoring/kafka/KafkaProducerConfig.java
+++ spring-boot/spring-boot-kafka/src/main/java/io/reflectoring/kafka/KafkaProducerConfig.java
@@ -40,6 +40,7 @@ class KafkaProducerConfig {
 		Map<String, Object> props = new HashMap<>();
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		return props;
 	}
 
